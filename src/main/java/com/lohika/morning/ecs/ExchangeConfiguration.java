82f7dadecaebcfba697f7def8914071668f42ba1
Ivan Verhun <902581+iverhun@users.noreply.github.com>
2018-01-11 15:47:17 +0200

    Added endpoint to send dummy email via exchange. Added spring profiles and externalized properties from the git repo

---
+++ src/main/java/com/lohika/morning/ecs/ExchangeConfiguration.java
@@ -0,0 +1,66 @@
+package com.lohika.morning.ecs;
+
+import lombok.extern.slf4j.Slf4j;
+import microsoft.exchange.webservices.data.autodiscover.exception.AutodiscoverLocalException;
+import microsoft.exchange.webservices.data.core.ExchangeService;
+import microsoft.exchange.webservices.data.core.enumeration.misc.ExchangeVersion;
+import microsoft.exchange.webservices.data.credential.ExchangeCredentials;
+import microsoft.exchange.webservices.data.credential.WebCredentials;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.URI;
+
+@Configuration
+@Slf4j
+public class ExchangeConfiguration {
+
+    @Value("${exchange.username}")
+    private String user;
+
+    @Value("${exchange.password}")
+    private String pwd;
+
+    @Value("${exchange.trace.enabled}")
+    private boolean exchangeTraceEnabled;
+
+    @Value("${exchange.default-url}")
+    private String exchangeServiceDefaultUrl;
+
+    @Value("${exchange.autodiscovery.enabled}")
+    private boolean exchangeAutodiscoveryEnabled;
+
+    @Value("${spring.config.location}")
+    private String scl;
+
+    @Bean
+    public ExchangeService exchangeService() throws Exception {
+        ExchangeService service = new ExchangeService(ExchangeVersion.Exchange2010_SP2);
+
+        ExchangeCredentials credentials = new WebCredentials(user, pwd);
+        service.setCredentials(credentials);
+
+        service.setTraceListener((traceType, traceMessage) -> log.info("Type: {}; Message: {}", traceType, traceMessage));
+        service.setTraceEnabled(exchangeTraceEnabled);
+
+        service.setUrl(new URI(exchangeServiceDefaultUrl));
+
+        if (exchangeAutodiscoveryEnabled) {
+            log.info("Auto-discovering exchange service URL...");
+
+            try {
+                service.autodiscoverUrl(user, url -> {
+                    log.info("Autodiscovered exchange service URL: {}", url.toLowerCase());
+                    return url.toLowerCase().startsWith("https://");
+                });
+            } catch (AutodiscoverLocalException e) {
+                log.warn("Exchange service autodiscovery failed. Using default exchange service URL");
+            }
+        }
+
+        log.info("Final exchange service URL: {}", service.getUrl());
+
+        return service;
+    }
+}
---
+++ src/main/java/com/lohika/morning/ecs/controller/EmailController.java
@@ -0,0 +1,26 @@
+package com.lohika.morning.ecs.controller;
+
+import com.lohika.morning.ecs.model.request.EmailRequest;
+import microsoft.exchange.webservices.data.core.ExchangeService;
+import microsoft.exchange.webservices.data.core.service.item.EmailMessage;
+import microsoft.exchange.webservices.data.property.complex.MessageBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmailController {
+
+    @Autowired
+    private ExchangeService exchangeService;
+
+    @PostMapping("email")
+    public void sendTestEmail(@RequestBody EmailRequest request) throws Exception {
+        EmailMessage msg = new EmailMessage(exchangeService);
+        msg.setSubject(request.getSubject());
+        msg.setBody(MessageBody.getMessageBodyFromText(request.getContent()));
+        msg.getToRecipients().add(request.getTo());
+        msg.send();
+    }
+}
