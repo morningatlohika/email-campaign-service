cfa5f1505f159abd698e9f3753e0a5fb97772029
Ihor Banadiga <banadiga@gmail.com>
2018-07-10 15:14:09 +0300

    campaign preview warning added

--- src/main/java/com/lohika/morning/ecs/domain/campaign/CampaignPreviewService.java
+++ src/main/java/com/lohika/morning/ecs/domain/campaign/CampaignPreviewService.java
@@ -8,8 +8,16 @@ import com.lohika.morning.ecs.domain.settings.SettingsService;
 import org.antlr.stringtemplate.StringTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.codehaus.groovy.runtime.InvokerHelper.asList;
+
+import static java.util.stream.Collectors.toSet;
 
 @Service
 @RequiredArgsConstructor
@@ -29,6 +37,21 @@ public class CampaignPreviewService {
     campaign.setBody(body);
 
     return campaign;
+  }
+
+  public Set<String> getNotSetKey(Long id) {
+    Campaign campaign = campaignService.findOne(id);
+    Map<String, String> variable = getVariable(campaign);
+
+    String body = campaign.getBody();
+    String[] strings = body.split("\\$");
+
+    AtomicInteger index = new AtomicInteger(0);
+    List<String> list = new ArrayList<String>(asList(strings));
+    return list.stream()
+        .filter(p -> index.incrementAndGet() % 2 == 0)
+        .filter(key -> variable.get(key) == null)
+        .collect(toSet());
   }
 
   private HashMap<String, String> getVariable(Campaign campaign) {
