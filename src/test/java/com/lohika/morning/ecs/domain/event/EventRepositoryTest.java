8a6ff4e03c038b42298f4aaa0ba2233178c4fd33
Ivan Verhun <ivan.verhun@gmail.com>
2018-08-15 00:12:51 +0300

    added /api/events endpoint

--- src/test/java/com/lohika/morning/ecs/domain/event/EventRepositoryTest.java
+++ src/test/java/com/lohika/morning/ecs/domain/event/EventRepositoryTest.java
@@ -7,7 +7,12 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -34,5 +39,32 @@ public class EventRepositoryTest extends BaseTest {
     assertEquals(event1.getName(), foundEvent.getName());
     assertEquals(event1.getDescription(), foundEvent.getDescription());
     assertEquals(event1.getDate(), foundEvent.getDate());
+  }
+
+  @Test
+  public void getAll_empty() {
+    // Given
+
+    // When
+    List<MorningEvent> all = eventRepository.findAll();
+
+    // Then
+    assertEquals(0, eventRepository.count());
+    assertEquals(0, all.size());
+  }
+
+  @Test
+  public void findByDate_empty() {
+    // Given
+    MorningEvent event1 = given.event("Golang Morning")
+        .withDescription("Another interesting event from Morning@Lohika will take place this December, 16. ")
+        .save();
+
+    // When
+    MorningEvent all = eventRepository.findByDate(LocalDate.now());
+
+    // Then
+    assertEquals(1, eventRepository.count());
+    assertNull(all);
   }
 }
