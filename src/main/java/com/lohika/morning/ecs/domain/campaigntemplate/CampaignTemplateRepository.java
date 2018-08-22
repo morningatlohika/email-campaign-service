d5ca81e073096098eb7ce0b782a4b09c93003e7c
Ihor Banadiga <banadiga@gmail.com>
2018-08-22 14:38:58 +0300

    add app state page

--- src/main/java/com/lohika/morning/ecs/domain/campaigntemplate/CampaignTemplateRepository.java
+++ src/main/java/com/lohika/morning/ecs/domain/campaigntemplate/CampaignTemplateRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import java.util.List;
 
 public interface CampaignTemplateRepository extends PagingAndSortingRepository<CampaignTemplate, Long> {
+  long countByReady(boolean ready);
+
   List<CampaignTemplate> findAll(Sort sort);
 
   List<CampaignTemplate> findByNameContainingIgnoringCaseOrSubjectContainingIgnoringCase(String email, String subject);
